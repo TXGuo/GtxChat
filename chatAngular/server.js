00d07c2c0a3a74f0c1471ff9ecd2f5bc1eab7e13
guotianxing <guotianxing@sina.cn>
2016-06-12 22:55:16 +0800

    Update server.js

--- chatAngular/server.js
+++ chatAngular/server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var test = '123';
 app.use(express.static(__dirname));
-
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
